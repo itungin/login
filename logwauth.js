17c5c72ae3c78b651f8d879d75a1652e0ee0801b
WaskithoCito.A <114119615+WaskithoCitoAdiwiguno@users.noreply.github.com>
2024-11-09 18:29:40 +0700

    Add files via upload

---
+++ logwauth.js
@@ -0,0 +1,13 @@
+import {
+    qrController,
+    deleteCookie,
+  } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
+  import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
+  
+  wauthparam.auth_ws = "d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
+  wauthparam.keyword = "  aHR0cHM6Ly93YS5tZS82Mjg1MTU3OTc5NzU5P3RleHQ9d2g0dDVhdXRoMA==";
+  wauthparam.tokencookiehourslifetime = 18;
+  wauthparam.redirect = "https://itung.in.my.id/dashboard/";
+  deleteCookie(wauthparam.tokencookiename);
+  qrController(wauthparam);
+  
