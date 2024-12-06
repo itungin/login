b921946622577d832419f5540fbff33cc5b1c455
Wangjarimm <karamissuu@gmail.com>
2024-12-06 16:57:31 +0700

    Update redirect URL in wauthparam to use relative path

--- qr.js
+++ qr.js
@@ -4,6 +4,6 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg1OTIyMDI0MDA3P3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
-wauthparam.redirect ="https://itung.in.my.id/dashboard/"
+wauthparam.redirect ="/auth"
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
